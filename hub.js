f76eb606c69f7c316f496a17a9943c12eaf646f2
Yasminemsnu <yasminemanuele309@gmail.com>
2026-06-29 16:06:04 -0300

    Update hub.js

--- hub.js
+++ hub.js
@@ -11,3 +11,62 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 200);
 
 });
+
+
+
+
+const curiosidades = [
+
+    "O ENIAC pesava cerca de 30 toneladas.",
+
+    "Ada Lovelace escreveu o primeiro algoritmo da história.",
+
+    "O primeiro disco rígido da IBM pesava mais de uma tonelada.",
+
+    "Um smartphone atual possui muito mais poder de processamento que o ENIAC.",
+
+    "Charles Babbage é conhecido como o pai da computação.",
+
+    "Alan Turing ajudou a decifrar códigos durante a Segunda Guerra Mundial.",
+
+    "O Ábaco foi criado há mais de 4 mil anos."
+
+];
+
+function mostrarCuriosidade(){
+
+    const numero = Math.floor(Math.random() * curiosidades.length);
+
+    document.getElementById("curiosidade").innerHTML = curiosidades[numero];
+
+}
+
+
+
+let palavra = "";
+
+document.addEventListener("keydown", function(event){
+
+    palavra += event.key.toUpperCase();
+
+    if(palavra.length > 5){
+        palavra = palavra.slice(-5);
+    }
+
+    if(palavra === "ENIAC"){
+
+        let aviso = document.getElementById("easterEgg");
+
+        aviso.style.display = "block";
+
+        setTimeout(function(){
+
+            aviso.style.display = "none";
+
+        },4000);
+
+        palavra = "";
+
+    }
+
+});
